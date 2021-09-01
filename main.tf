b654e35e2750a038fc7d840a95e7b5bc503662dd
Cyril Feraudet <cyril@feraudet.com>
2021-09-01 10:53:59 +0200

    Add missing Resource-based policy

--- main.tf
+++ main.tf
@@ -85,6 +85,14 @@ resource "aws_iam_role" "this" {
   tags = var.tags
 }
 
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
+
 resource "aws_cloudwatch_log_group" "this" {
   name = "/aws/lambda/${var.name}"
   tags = var.tags
